74973b01bbc57856aadde38c65baf3c3500b9ab7
Mohan Raj VD <vdmohanraj007@gmail.com>
2026-08-13 11:42:44 +0530

    Add files via upload

--- Experiments/Word Break/word_break.py
+++ Experiments/Word Break/word_break.py
@@ -1,11 +1,13 @@
-s = input("Enter string: ")
-words = ["i", "like", "sam", "sung", "samsung", "mobile"]
-n = len(s)
-dp = [False] * (n + 1)
-dp[0] = True
-for i in range(1, n + 1):
-    for j in range(i):
-        if dp[j] and s[j:i] in words:
-            dp[i] = True
-            break
-print("Can be segmented:", dp[n])
+def wordBreak(s, words):
+    n = len(s)
+    dp = [False] * (n + 1)
+    dp[0] = True
+    for i in range(1, n + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in words:
+                dp[i] = True
+                break
+    return dp[n]
+s = "ilike"
+words = {"i", "like", "ice", "cream"}
+print("Can be segmented:", wordBreak(s, words))
